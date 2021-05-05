3537f9480623a5148ddf16c20e819a833af81c30
tmh9x <thomas.hermanu@gmail.com>
2021-05-05 14:31:43 +0200

    ✨ fetch api

---
+++ utils/api.js
@@ -0,0 +1,5 @@
+export function getCharacters() {
+  return fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((response) => response.results);
+}
